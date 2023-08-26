7de2d7d8918170497d4565c101683449a6364f4b
johanncatalla <catallajohann@gmail.com>
2023-08-26 21:42:30 +0800

    username auth

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+import json
+from django.contrib.auth.models import User
+
+
+class UsernameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+
+        if not str(username).isalnum():
+            return JsonResponse({'username_error': 'username should only contain alphanumeric characters'}, status=400)
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error': 'username is taken'}, status=409)
+        
+        return JsonResponse({'usernamne_valid': True})
+
 
 class RegistrationView(View):
     def get(self, request):
