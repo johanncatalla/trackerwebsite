0d3c6cbaa2bceabf6f5211faa331dfd9fbe1bbe1
johanncatalla <catallajohann@gmail.com>
2023-08-26 22:44:03 +0800

    register auth

--- authentication/views.py
+++ authentication/views.py
@@ -4,7 +4,19 @@ from django.http import JsonResponse
 from django.contrib.auth.models import User
 import json
 from django.contrib.auth.models import User
+from validate_email import validate_email
 
+class EmailValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        email = data['email']
+
+        if not validate_email(email):
+            return JsonResponse({'email_error': 'email is invalid'}, status=400)
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error': 'email is taken'}, status=409)
+        
+        return JsonResponse({'email_valid': True})
 
 class UsernameValidationView(View):
     def post(self, request):
@@ -17,7 +29,6 @@ class UsernameValidationView(View):
             return JsonResponse({'username_error': 'username is taken'}, status=409)
         
         return JsonResponse({'usernamne_valid': True})
-
 
 class RegistrationView(View):
     def get(self, request):
--- trackerwebsite/static/js/register.js
+++ trackerwebsite/static/js/register.js
@@ -1,9 +1,14 @@
-const usernameField=document.querySelector('#usernameField');
-const feedBackArea=document.querySelector('.invalid_feedback')
+const usernameField=document.querySelector("#usernameField");
+const feedBackArea=document.querySelector(".invalid_feedback");
+const emailField=document.querySelector("#emailfield");
+const EmailfeedBackArea=document.querySelector(".EmailFeedBackArea");
+const usernameSuccessOutput=document.querySelector(".usernameSuccessOutput");
+
 usernameField.addEventListener('keyup', (e) => {
-    console.log('7777', 7777);
     const usernameVal=e.target.value;
-    
+    usernameSuccessOutput.style.display="block";
+    usernameSuccessOutput.textContent=`Checking ${usernameVal}`;
+
     usernameField.classList.remove("is-invalid");
     feedBackArea.style.display="none";
 
@@ -15,10 +20,34 @@ usernameField.addEventListener('keyup', (e) => {
             .then(res=>res.json())
             .then((data) => {
                 console.log("data", data);
+                usernameSuccessOutput.style.display="none";
                 if (data.username_error){
                     usernameField.classList.add("is-invalid");
                     feedBackArea.style.display="block";
                     feedBackArea.innerHTML=`<p>${data.username_error}<\p>`
+                }
+            });
+    }
+});
+
+emailField.addEventListener('keyup', (e) => {
+    const emailVal=e.target.value;
+    
+    emailField.classList.remove("is-invalid");
+    EmailfeedBackArea.style.display="none";
+
+    if (emailVal.length>0) {
+        fetch("/authentication/validate-email", {
+            body:JSON.stringify({ email: emailVal }), 
+            method: "POST",
+        })
+            .then(res=>res.json())
+            .then((data) => {
+                console.log("data", data);
+                if (data.email_error){
+                    emailField.classList.add("is-invalid");
+                    EmailfeedBackArea.style.display="block";
+                    EmailfeedBackArea.innerHTML=`<p>${data.email_error}<\p>`
                 }
             });
     }
