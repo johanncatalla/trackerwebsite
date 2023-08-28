85a17c289d4d4a7ffae50ad80aecca2b7a0a38cb
johanncatalla <catallajohann@gmail.com>
2023-08-28 15:34:28 +0800

    currency

--- userpreferences/views.py
+++ userpreferences/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.conf import settings
+import json
+# 
+import os
 
 def index(request):
-    return render(request, 'preferences/index.html')
+    currency_data = []
+    file_path = os.path.join(settings.BASE_DIR, 'currencies.json')
+    
+    with open(file_path, 'r') as json_file:
+        data = json.load(json_file)
+
+        for key, value in data.items():
+            currency_data.append({'name': key, 'value': value})
+
+    return render(request, 'preferences/index.html', {'currencies': currency_data})
